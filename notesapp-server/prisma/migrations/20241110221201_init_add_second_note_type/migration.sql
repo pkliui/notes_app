1586fc0f20153af00719d16fc1df0e8630688664
pkliui <kliuiev@pm.me>
2024-11-11 00:25:05 +0200

    add migrations

---
+++ notesapp-server/prisma/migrations/20241110221201_init_add_second_note_type/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Note_Second_type" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "Note_Second_type_pkey" PRIMARY KEY ("id")
+);
---
+++ notesapp-server/prisma/migrations/20241110221424_back_to_one_note/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Note_Second_type` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Note_Second_type";
