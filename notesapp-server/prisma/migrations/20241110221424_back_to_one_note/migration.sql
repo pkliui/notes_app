23a6ad7c7dd4c1ccdbf2073ab66354c7e7a7ab8d
pkliui <44207525+pkliui@users.noreply.github.com>
2026-03-27 16:27:48 +0200

    upgrade prisma and refactor accordingly

--- notesapp-server/prisma/migrations/20241110221424_back_to_one_note/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Note_Second_type` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Note_Second_type";
